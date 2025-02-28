b65935ebd414045b11630cf787bc8f60fff48b9f
Atanas Dimitrov <hi.atanasdim@gmail.com>
2025-02-28 14:43:30 +0000

    Code clean

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,4 +1,4 @@
-import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import {
   Lightformer,
   Text,
@@ -9,8 +9,6 @@ import {
   OrbitControls,
   PerspectiveCamera,
   Sphere,
-  Cone,
-  useTexture,
 } from "@react-three/drei";
 import {
   Bloom,
@@ -18,7 +16,6 @@ import {
   FXAA,
   TiltShift2,
 } from "@react-three/postprocessing";
-import { RGBELoader } from "three-stdlib";
 
 import { useEffect, useRef, type FC } from "react";
 import {
@@ -29,6 +26,8 @@ import {
   Vector3,
 } from "three";
 
+const RADIUS = 4;
+
 const Scene = () => (
   <Canvas dpr={[2, 3]} gl={{}} shadows={false}>
     <OrbitControls />
@@ -38,8 +37,6 @@ const Scene = () => (
     <spotLight position={[20, 20, 10]} penumbra={1} castShadow angle={0.2} />
 
     <Torus />
-    {/* <Bubble /> */}
-    {/* <Pyramid /> */}
 
     <ContactShadows
       scale={100}
@@ -66,38 +63,20 @@ export default Scene;
 const SceneEnvironment = () => {
   return (
     <>
-      <Environment
-        environmentIntensity={2.75}
-        preset="studio"
-        // background
-      >
+      <Environment environmentIntensity={2.75} preset="studio">
         <Lightformer intensity={8} position={[7, 5, 2]} scale={[4, 7, 1]} />
       </Environment>
     </>
   );
 };
 
-const Bubble = () => {
-  return (
-    <Sphere args={[1, 32, 32]} position={new Vector3(0, 0, 0)}>
-      <MeshTransmissionMaterial
-        thickness={0}
-        resolution={window.innerWidth * 2}
-        color={"#ffffff"}
-      />
-    </Sphere>
-  );
-};
-
 const Torus: FC = () => {
-  const ref1 = useRef<Mesh>(null);
-  const ref2 = useRef<Mesh>(null);
-  const ref3 = useRef<Mesh>(null);
-  const radius = 4; // Circular path radius
+  const star1Ref = useRef<Mesh>(null);
+  const star2Ref = useRef<Mesh>(null);
+  const star3Ref = useRef<Mesh>(null);
 
   const envMap = useLoader(TextureLoader, "/gradients-seamless-1.jpg");
 
-  // Ensure correct environment map encoding
   useEffect(() => {
     if (envMap) {
       envMap.mapping = EquirectangularReflectionMapping; // Important for reflections
@@ -105,20 +84,20 @@ const Torus: FC = () => {
   }, [envMap]);
 
   useFrame(({ clock }) => {
-    if (ref1.current) {
+    if (star1Ref.current) {
       const t = clock.getElapsedTime(); // Get time
-      ref1.current.position.x = Math.cos(t) * radius;
-      ref1.current.position.y = Math.sin(t) * radius;
+      star1Ref.current.position.x = Math.cos(t) * RADIUS;
+      star1Ref.current.position.y = Math.sin(t) * RADIUS;
     }
-    if (ref2.current) {
+    if (star2Ref.current) {
       const t = clock.getElapsedTime() * 0.7; // Get time
-      ref2.current.position.x = Math.cos(t) * radius;
-      ref2.current.position.y = Math.sin(t) * radius; // Same position as ref
+      star2Ref.current.position.x = Math.cos(t) * RADIUS;
+      star2Ref.current.position.y = Math.sin(t) * RADIUS;
     }
-    if (ref3.current) {
+    if (star3Ref.current) {
       const t = clock.getElapsedTime() * 0.5; // Get time
-      ref3.current.position.x = Math.cos(t) * radius;
-      ref3.current.position.y = Math.sin(t) * radius; // Same position as ref
+      star3Ref.current.position.x = Math.cos(t) * RADIUS;
+      star3Ref.current.position.y = Math.sin(t) * RADIUS;
     }
   });
 
@@ -141,51 +120,34 @@ const Torus: FC = () => {
         <MeshTransmissionMaterial
           thickness={0.01}
           color="#e9fcff"
-          // color="#ffa7d3"
           resolution={window.innerWidth * 2}
         />
       </mesh>
-      <Sphere position={[4, 0, 0]} args={[0.1]} scale={[1, 1, 1]} ref={ref1}>
+      <Sphere
+        position={[4, 0, 0]}
+        args={[0.1]}
+        scale={[1, 1, 1]}
+        ref={star1Ref}
+      >
         <meshStandardMaterial emissive="#ff82d1" emissiveIntensity={48} />
       </Sphere>
-      <Sphere position={[4, 0, 0]} args={[0.05]} scale={[1, 1, 1]} ref={ref2}>
+      <Sphere
+        position={[4, 0, 0]}
+        args={[0.05]}
+        scale={[1, 1, 1]}
+        ref={star2Ref}
+      >
         <meshStandardMaterial emissive="pink" emissiveIntensity={38} />
       </Sphere>
-      <Sphere position={[4, 0, 0]} args={[0.025]} scale={[1, 1, 1]} ref={ref3}>
+      <Sphere
+        position={[4, 0, 0]}
+        args={[0.025]}
+        scale={[1, 1, 1]}
+        ref={star3Ref}
+      >
         <meshStandardMaterial emissive="#cd7dff" emissiveIntensity={33} />
       </Sphere>
     </group>
-  );
-};
-
-const Pyramid: FC = () => {
-  const ref = useRef<Mesh>(null);
-
-  useFrame(({ clock }) => {
-    if (ref.current) {
-      const t = clock.getElapsedTime();
-
-      //rotate 360 degrees every 2 seconds
-      ref.current.rotation.y += 0.015;
-    }
-  });
-
-  return (
-    <>
-      <Cone ref={ref} args={[1.5, 2, 4]} position={[0, 0, 0]}>
-        <MeshTransmissionMaterial
-          thickness={1}
-          resolution={window.innerWidth * 2}
-          color={"#d7f0f9"}
-          // backside
-          backsideThickness={0.01}
-          // transmission={0.98}
-          // metalness={0.11}
-          // roughness={0.12}
-          chromaticAberration={0.25}
-        />
-      </Cone>
-    </>
   );
 };
 
